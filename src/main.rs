404dd0f7073edf59b181d31fd9afcae7f267aad2
yiz <ychouy@126.com>
2024-12-08 00:49:55 +0800

    translate from cpp to rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod add;
+use crate::add;
 
 fn main() {
     let num1 = 5;
     let num2 = 3;
-    let result = add::add(num1, num2);
+    let result = add(num1, num2);
     println!("The sum of {} and {} is {}", num1, num2, result);
 }
